e24e6be489e3d63834e52c4f7b7d966b13908006
Brandonnogithub <2275135452@qq.com>
2020-09-27 23:44:09 +0800

    update gitignore

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # annotation_squad
+
 this is a tool to annotate dataset
+
+## Requirement
+
+* python
+
+* flask
+
+* stanza (aotu install pytorch, only use in process data, not necessary for annotator)
+
+## Usage
+
+`python app.py`
--- app.py
+++ app.py
@@ -76,11 +76,11 @@ def savepost():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--local", "-l", action='store_true', help="run local")
+    parser.add_argument("--server", "-s", action='store_true', help="run as a server")
     parser.add_argument("--port", "-p", default=5000, type=int, help="runnning port")
     args = parser.parse_args()
 
-    if args.local:
-        app.run(host='127.0.0.1', port=args.port, debug=True)
-    else:
+    if args.server:
         app.run(host='0.0.0.0', port=args.port)
+    else:
+        app.run(host='127.0.0.1', port=args.port, debug=True)
